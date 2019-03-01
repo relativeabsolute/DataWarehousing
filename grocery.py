d5d5fffcf16004a2faf29117ba637c33088fd231
Johan Burke <jburke4126@gmail.com>
2019-03-01 11:31:12 -0500

    Added inventory

--- grocery.py
+++ grocery.py
@@ -8,11 +8,14 @@ import datetime
 
 def manage_tables(cursor):
     cursor.execute('''DROP TABLE IF EXISTS products''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS products
+    cursor.execute('''CREATE TABLE products
         (Manufacturer TEXT, ProductName TEXT, Size REAL, SizeUnit TEXT, ItemType TEXT, SKU INT, BasePrice REAL)''')
 
     cursor.execute('''DROP TABLE IF EXISTS sales_record''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS sales_record (Date TEXT, CustomerNum INT, SKU INT, SalePrice REAL)''')
+    cursor.execute('''CREATE TABLE sales_record (Date TEXT, CustomerNum INT, SKU INT, SalePrice REAL)''')
+
+    cursor.execute('''DROP TABLE IF EXISTS inventory''')
+    cursor.execute('''CREATE TABLE inventory (SKU INT, NumberOnHand INT, TotalCasesOrdered INT, ExpectedDaily INT)''')
 
 
 # c is database cursor
@@ -45,61 +48,135 @@ def import_products(c, filename):
 
 def get_partial_sale(c, type_name, price_multiplier):
     record = get_item_type(c, type_name)
-    return {'SKU': record[5], 'SalePrice': record[6] * price_multiplier}
+    if record is None:
+        return None
+    return {'SKU': record[0], 'SalePrice': record[1] * price_multiplier}
 
 
 # get a random row that is the given type
+# returns [SKU, BasePrice]
 def get_item_type(c, type_name):
-    c.execute('''SELECT * FROM products where ItemType = ?''', [type_name])
+    c.execute('''SELECT products.SKU, products.BasePrice
+     FROM products join inventory on products.SKU = inventory.SKU
+     where ItemType = ? and NumberOnHand > 0''', [type_name])
     rows = c.fetchall()
+    if len(rows) == 0:
+        return None
     return rows[random.randrange(0, len(rows))]
 
 
 # Added this to get the non probability products, not sure if this is correct
 def non_probability_items(c, number_items, price_multiplier):
-    c.execute('''SELECT * FROM products where ItemType NOT IN ('Milk', 'Cereal', 'Baby Food', 'Diapers', 'Peanut Butter', 'Jelly/Jam')''')
+    c.execute('''SELECT products.SKU, products.BasePrice
+        FROM products join inventory on products.SKU = inventory.SKU
+        where ItemType NOT IN ('Milk', 'Cereal', 'Baby Food', 'Diapers', 'Peanut Butter', 'Jelly/Jam'
+        and NumberOnHand > 0)''')
     rows = c.fetchall()
     records = random.sample(rows,k=number_items)
-    return [{'SKU': record[5], 'SalePrice': record[6] * price_multiplier} for record in records]
+    return [{'SKU': record[0], 'SalePrice': record[1] * price_multiplier} for record in records]
 
 
-# returns partial (SKU and sale price) sales records of items bought with probability
-def do_probability_sales(c, price_multiplier):
-    result = []
-    probabilities = [{ 'type': 'Milk', 'prob': 0.7, 'yes': {'type': 'Cereal', 'prob': 0.5},
+probabilities = [{ 'type': 'Milk', 'prob': 0.7, 'yes': {'type': 'Cereal', 'prob': 0.5},
                       'no': {'type': 'Cereal', 'prob': 0.05}},
                      {'type': 'Baby Food', 'prob': 0.2, 'yes': {'type': 'Diapers', 'prob': 0.8},
                       'no': {'type': 'Diapers', 'prob': 0.01}},
                      {'type': 'Bread', 'prob': 0.5},
                      {'type': 'Peanut Butter', 'prob': 0.1, 'yes': {'type': 'Jelly/Jam', 'prob': 0.9},
-                      'no': {'type': 'Jelly/Jam', 'prob': 0.05}}
-                     ]
-    counter = 0
-    for item_dict in probabilities:
-        chance = random.random()
-        if chance <= item_dict['prob']:
+                      'no': {'type': 'Jelly/Jam', 'prob': 0.05}}]
 
-            result.append(get_partial_sale(c, item_dict['type'], price_multiplier))
-            if 'yes' in item_dict:
-                chance = random.random()
-                if chance <= item_dict['yes']['prob']:
-                    result.append(get_partial_sale(c, item_dict['yes']['type'], price_multiplier))
-        elif 'no' in item_dict:
-            chance = random.random()
-            if chance <= item_dict['no']['prob']:
-                result.append(get_partial_sale(c, item_dict['no']['type'], price_multiplier))
+min_customers = 1000
+max_customers = 1040
+weekend_increase = 50
+price_multiplier = 1.05
+max_items = 70
+
+
+def initial_inventory(c):
+    expected = read_expected_daily("expected_daily.csv")
+    result = []
+    for row in c.execute('''SELECT SKU, ItemType FROM products'''):
+        row_dict = {'SKU': row[0]}
+        type_name = row[1]
+        if row[1] not in expected:
+            type_name = 'regular'
+        multiplier = 3
+        if row[1] == 'Milk':
+            multiplier = 1.5
+        cases = int(multiplier * expected[type_name]) // 12 + 1
+        #row_dict['ItemType'] = type_name
+        row_dict['TotalCasesOrdered'] = cases
+        row_dict['NumberOnHand'] = cases * 12
+        row_dict['ExpectedDaily'] = expected[type_name]
+        result.append(row_dict)
+    c.executemany('''INSERT INTO inventory VALUES (:SKU, :NumberOnHand, :TotalCasesOrdered, :ExpectedDaily)''', result)
+
+
+def expected_sales():
+    expected_customers = (min_customers + max_customers) / 2
+    result = {}
+    for item_dict in probabilities:
+        prob = item_dict['prob']
+        result[item_dict['type']] = expected_customers * prob
+        if 'yes' in item_dict:
+            result[item_dict['yes']['type']] = expected_customers * prob * item_dict['yes']['prob']
+        if 'no' in item_dict:
+            result[item_dict['no']['type']] = expected_customers * (1 - prob) * item_dict['no']['prob']
+    result['regular'] = expected_customers * (max_items / 2) / 66
     return result
 
 
-def do_sales(c):
-    min_customers = 1000
-    max_customers = 1040
-    weekend_increase = 50
-    price_multiplier = 1.05
-    max_items = 70
+# returns partial (SKU and sale price) sales records of items bought with probability
+def do_probability_sales(c, price_multiplier):
+    result = []
+    for item_dict in probabilities:
+        chance = random.random()
+        if chance <= item_dict['prob']:
+            partial = get_partial_sale(c, item_dict['type'], price_multiplier)
+            if partial is not None:
+                result.append(partial)
+            if 'yes' in item_dict:
+                chance = random.random()
+                if chance <= item_dict['yes']['prob']:
+                    partial = get_partial_sale(c, item_dict['yes']['type'], price_multiplier)
+                    if partial is not None:
+                        result.append(partial)
+        elif 'no' in item_dict:
+            chance = random.random()
+            if chance <= item_dict['no']['prob']:
+                partial = get_partial_sale(c, item_dict['no']['type'], price_multiplier)
+                if partial is not None:
+                    result.append(partial)
+    return result
 
+
+def do_deliveries(c):
+    for row in c.execute('''SELECT products.SKU, NumberOnHand, ExpectedDaily from products
+            join inventory on products.SKU = inventory.SKU
+            where ItemType <> 'Milk' and NumberOnHand < 3 * ExpectedDaily'''):
+        needed = 3 * row[2] - row[1] # 3 * Expected - NumberOnHand
+        cases = needed // 12 + 1
+        ordered = cases * 12
+        c.execute('''UPDATE inventory
+            SET NumberOnHand = NumberOnHand + ?,
+            TotalCasesOrdered = TotalCasesOrdered + ?
+            WHERE SKU = ?''', [ordered, cases, row[0]])
+    for row in c.execute('''SELECT products.SKU, NumberOnHand, ExpectedDaily from products
+            join inventory on products.SKU = inventory.SKU
+            where ItemType = 'Milk' and NumberOnHand < 1.5 * ExpectedDaily'''):
+        needed = int(1.5 * row[2]) - row[1]
+        cases = needed // 12 + 1
+        ordered = cases * 12
+        c.execute('''UPDATE inventory
+                    SET NumberOnHand = NumberOnHand + ?,
+                    TotalCasesOrdered = TotalCasesOrdered + ?
+                    WHERE SKU = ?''', [ordered, cases, row[0]])
+
+
+def do_sales(c):
     for i in range(365):
         print("Day {}".format(i))
+        if i % 3 == 0:
+            do_deliveries(c)
         num_customers = random.randint(min_customers, max_customers) # min_customers <= num_customers <= max_customers
         if i % 7 == 0 or i % 7 == 6: # jan 1 2017 was a sunday
             num_customers += weekend_increase
@@ -119,8 +196,19 @@ def do_sales(c):
             for sale_index in range(len(current_sales)):
                 current_sales[sale_index].update({'Date': (datetime.date(2017, 1, 1) + datetime.timedelta(days=i)).isoformat(),
                                          'CustomerNum': customer + 1})
+            inventory_update = [[item['SKU']] for item in current_sales]
+            c.executemany('''UPDATE inventory set NumberOnHand = NumberOnHand - 1
+                where SKU = ?''', inventory_update)
         # add sales per customer
             c.executemany('''INSERT INTO sales_record VALUES (:Date, :CustomerNum, :SKU, :SalePrice)''', current_sales)
+
+
+def read_expected_daily(filename):
+    result = []
+    with open(filename, 'r', newline='') as filehandle:
+        reader = csv.DictReader(filehandle)
+        result = {row['Type'] : int(row['Number']) for row in reader}
+    return result
 
 
 def compute_summaries(c):
@@ -138,7 +226,9 @@ def compute_summaries(c):
     print("Total items bought: {}".format(c.fetchone()))
 
     # top 10 selling items with counts
-    c.execute('''SELECT SKU, COUNT(SKU) from sales_record GROUP BY SKU ORDER BY COUNT(SKU) LIMIT 10''')
+    c.execute('''SELECT products.SKU, COUNT(products.SKU), NumberOnHand, TotalCasesOrdered from sales_record
+    join inventory on products.SKU = inventory.SKU
+    GROUP BY SKU ORDER BY COUNT(SKU) LIMIT 10''')
     print("Top 10 selling items: {}".format(c.fetchall()))
 
 
@@ -151,7 +241,8 @@ if __name__ == '__main__':
         c = conn.cursor()
         manage_tables(c)
         import_products(c, sys.argv[1])
+        initial_inventory(c)
         conn.commit()
         do_sales(c)
         conn.commit()
-        compute_summaries(c)
+        #compute_summaries(c)
